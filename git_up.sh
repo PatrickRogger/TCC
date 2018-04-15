4935cd05678bb8874f0d5c2effb7dcb9b0f22e73
Patrick Rogger Garcia <patrick_rogger@hotmail.com>
2018-04-14 22:04:59 -0300

    Texto parcial

--- git_up.sh
+++ git_up.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 cd /home/patrick/TCC/TCC-git
-cp documentos/texto_parcial_2018-05-16/source_latex_utf/texto_parcial_TCC1.pdf ultima_versao.pdf
+cp documentos/texto_parcial_2018-05-16/source_utf-8/texto_parcial_2018-05-16.pdf ultima_versao.pdf
 
 git status
 git add *
 
 echo "----Assunto Commit:"
-echo " "
 read texto_commit
 
 git commit -m "$texto_commit"
